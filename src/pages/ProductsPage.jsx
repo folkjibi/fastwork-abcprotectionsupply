cebd4a67bd2d36968bfb512aec76fa17b6d7e333
Anupong lewtoksan <comnet58.33621@gmail.com>
2024-07-28 19:33:41 +0700

    Update ProductPage

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,13 @@
 import React, { useState } from 'react'
-import { Dialog, DialogBackdrop, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Dialog, DialogBackdrop, DialogPanel, Disclosure, DisclosureButton, DisclosurePanel, } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import { FunnelIcon, MinusIcon, PlusIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 
 import IMG1 from '../assets/images/home/A0149467.jpg'
 import IMG2 from '../assets/images/home/A0148264.jpg'
 import IMG3 from '../assets/images/home/A0149476.jpg'
 import IMG4 from '../assets/images/home/A0158296.jpg'
-
-const sortOptions = [
-    { name: 'Most Popular', href: '#', current: true },
-    { name: 'Best Rating', href: '#', current: false },
-    { name: 'Newest', href: '#', current: false },
-    { name: 'Price: Low to High', href: '#', current: false },
-    { name: 'Price: High to Low', href: '#', current: false },
-]
 
 const subCategories = [
     { name: 'Totes', href: '#' },
@@ -26,18 +18,6 @@ const subCategories = [
 ]
 
 const filters = [
-    {
-        id: 'color',
-        name: 'Color',
-        options: [
-            { value: 'white', label: 'White', checked: false },
-            { value: 'beige', label: 'Beige', checked: false },
-            { value: 'blue', label: 'Blue', checked: true },
-            { value: 'brown', label: 'Brown', checked: false },
-            { value: 'green', label: 'Green', checked: false },
-            { value: 'purple', label: 'Purple', checked: false },
-        ],
-    },
     {
         id: 'category',
         name: 'Category',
@@ -148,11 +128,11 @@ const ProductsPage = () => {
                                             <div className="space-y-6">
                                                 {section.options.map((option, optionIdx) => (
                                                     <div key={option.value} className="flex items-center">
-                                                        <input 
-                                                            defaultValue={option.value} 
-                                                            defaultChecked={option.checked} 
-                                                            id={`filter-mobile-${section.id}-${optionIdx}`} 
-                                                            type="checkbox" 
+                                                        <input
+                                                            defaultValue={option.value}
+                                                            defaultChecked={option.checked}
+                                                            id={`filter-mobile-${section.id}-${optionIdx}`}
+                                                            type="checkbox"
                                                             className="w-4 h-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                                         />
                                                         <label htmlFor={`filter-mobile-${section.id}-${optionIdx}`} className="ml-3 min-w-0 flex-1 text-gray-500">
@@ -174,40 +154,6 @@ const ProductsPage = () => {
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900">Product</h1>
 
                         <div className="flex items-center">
-                            {/* <Menu as="div" className="relative inline-block text-left">
-                                <div>
-                                    <MenuButton className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                                        Sort
-                                        <ChevronDownIcon aria-hidden="true" className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
-                                    </MenuButton>
-                                </div>
-
-                                <MenuItems
-                                    transition
-                                    className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                                >
-                                    <div className="py-1">
-                                        {sortOptions.map((option) => (
-                                            <MenuItem key={option.name}>
-                                                <a
-                                                href={option.href}
-                                                className={classNames(
-                                                    option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                                                    'block px-4 py-2 text-sm data-[focus]:bg-gray-100',
-                                                )}
-                                                >
-                                                {option.name}
-                                                </a>
-                                            </MenuItem>
-                                        ))}
-                                    </div>
-                                </MenuItems>
-                            </Menu>
-
-                            <button type="button" className="-m-2 ml-5 p-2 text-gray-400 hover:text-gray-500 sm:ml-7">
-                                <span className="sr-only">View grid</span>
-                                <Squares2X2Icon aria-hidden="true" className="h-5 w-5" />
-                            </button> */}
                             <button
                                 type="button"
                                 onClick={() => setMobileFiltersOpen(true)}
@@ -229,42 +175,42 @@ const ProductsPage = () => {
                                 <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
                                     {subCategories.map((category) => (
                                         <li key={category.name}>
-                                        <a href={category.href}>{category.name}</a>
+                                            <a href={category.href}>{category.name}</a>
                                         </li>
                                     ))}
                                 </ul>
 
                                 {filters.map((section) => (
-                                <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
-                                    <h3 className="-my-3 flow-root">
-                                        <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                                            <span className="font-medium text-gray-900">{section.name}</span>
-                                            <span className="ml-6 flex items-center">
-                                                <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
-                                                <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
-                                            </span>
-                                        </DisclosureButton>
-                                    </h3>
-                                    <DisclosurePanel className="pt-6">
-                                        <div className="space-y-4">
-                                            {section.options.map((option, optionIdx) => (
-                                            <div key={option.value} className="flex items-center">
-                                                <input
-                                                    defaultValue={option.value}
-                                                    defaultChecked={option.checked}
-                                                    id={`filter-${section.id}-${optionIdx}`}
-                                                    name={`${section.id}[]`}
-                                                    type="checkbox"
-                                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                />
-                                                <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
-                                                    {option.label}
-                                                </label>
+                                    <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
+                                        <h3 className="-my-3 flow-root">
+                                            <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                                <span className="font-medium text-gray-900">{section.name}</span>
+                                                <span className="ml-6 flex items-center">
+                                                    <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
+                                                    <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
+                                                </span>
+                                            </DisclosureButton>
+                                        </h3>
+                                        <DisclosurePanel className="pt-6">
+                                            <div className="space-y-4">
+                                                {section.options.map((option, optionIdx) => (
+                                                    <div key={option.value} className="flex items-center">
+                                                        <input
+                                                            defaultValue={option.value}
+                                                            defaultChecked={option.checked}
+                                                            id={`filter-${section.id}-${optionIdx}`}
+                                                            name={`${section.id}[]`}
+                                                            type="checkbox"
+                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                                        />
+                                                        <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
+                                                            {option.label}
+                                                        </label>
+                                                    </div>
+                                                ))}
                                             </div>
-                                            ))}
-                                        </div>
-                                    </DisclosurePanel>
-                                </Disclosure>
+                                        </DisclosurePanel>
+                                    </Disclosure>
                                 ))}
                             </form>
 
