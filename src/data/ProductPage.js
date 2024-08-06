979b358926f7457a38c6e936b5c80470c4738147
Anupong lewtoksan <comnet58.33621@gmail.com>
2024-08-06 23:03:50 +0700

    commit product page

---
+++ src/data/ProductPage.js
@@ -0,0 +1,481 @@
+// fireman
+import IMG1 from "../assets/images/products/Group-one/spray.jpg";
+import IMG2 from "../assets/images/products/Group-one/fireman 2.2 ปอนด์.jpg";
+import IMG3 from "../assets/images/products/Group-one/fireman 5 ปอนด์.jpg";
+import IMG4 from "../assets/images/products/Group-one/fireman10ปอนด์.jpg";
+import IMG5 from "../assets/images/products/Group-one/fireman15ปอนด์.jpg";
+import IMG6 from "../assets/images/products/Group-one/fireman (AFFF Pre-Mix Foam) ขนาด 6 ลิตร.jpg";
+import IMG7 from "../assets/images/products/Group-one/fireman (AFFF Pre-Mix Foam) ขนาด 9 ลิตร.jpg";
+import IMG8 from "../assets/images/products/Group-one/fireman BF2000 2.2 ปอนด์.jpg";
+import IMG9 from "../assets/images/products/Group-one/fireman BF2000 5 ปอนด์.jpg";
+import IMG10 from "../assets/images/products/Group-one/fireman BF2000 10 ปอนด์.jpg";
+import IMG11 from "../assets/images/products/Group-one/fireman BF2000 15 ปอนด์.jpg";
+import IMG12 from "../assets/images/products/Group-one/fireman Co2 5 ปอนด์.jpg";
+import IMG13 from "../assets/images/products/Group-one/fireman Co2 10 ปอนด์.jpg";
+import IMG14 from "../assets/images/products/Group-one/fireman Co2 15 ปอนด์.jpg";
+import IMG15 from "../assets/images/products/Group-one/fireman (Water Gas) ขนาด 6 ลิตร.jpg";
+import IMG16 from "../assets/images/products/Group-one/fireman (Water Gas) ขนาด 9 ลิตร.jpg";
+import IMG17 from "../assets/images/products/Group-one/firemax BF2000 2.2 ปอนด์.jpg"
+import IMG18 from "../assets/images/products/Group-one/firemax5 ปอนด์.jpg";
+import IMG19 from "../assets/images/products/Group-one/firemax10 ปอนด์.jpg";
+import IMG20 from "../assets/images/products/Group-one/firemax15 ปอนด์.jpg";
+import IMG21 from "../assets/images/products/Group-one/firemax20 ปอนด์.jpg";
+import IMG22 from "../assets/images/products/Group-one/firemax BF2000 2.2 ปอนด์.jpg";
+import IMG23 from "../assets/images/products/Group-one/firemax BF2000 5 ปอนด์.jpg";
+import IMG24 from "../assets/images/products/Group-one/firemax BF2000 10 ปอนด์.jpg";
+import IMG25 from "../assets/images/products/Group-one/firemax BF2000 15 ปอนด์.jpg";
+import IMG26 from "../assets/images/products/Group-one/firemax Co2 5 ปอนด์.jpg";
+import IMG27 from "../assets/images/products/Group-one/firemax Co2 10 ปอนด์.jpg";
+import IMG28 from "../assets/images/products/Group-one/firemax Co2 15 ปอนด์.jpg";
+
+// sunny
+import IMG29 from "../assets/images/products/Group-two/sunny CU209CD4.jpg"
+import IMG30 from "../assets/images/products/Group-two/sunny SG209CD2.jpg"
+import IMG31 from "../assets/images/products/Group-two/sunny SG209CD4.jpg"
+
+// กรวยจราจร
+import IMG32 from "../assets/images/products/Group-three/กรวยจราจร 30cm.jpg"
+import IMG33 from "../assets/images/products/Group-three/กรวยจราจร 50cm.jpg"
+import IMG34 from "../assets/images/products/Group-three/กรวยจราจร 70cm.jpg"
+import IMG35 from "../assets/images/products/Group-three/กรวยจราจร 80cm.jpg"
+import IMG36 from "../assets/images/products/Group-three/กระจกโค้งจราจรแบบมีหมวก ขนาด 12 YAMADA.jpg"
+import IMG37 from "../assets/images/products/Group-three/กระจกโค้งจราจรแบบมีหมวก ขนาด 18 YAMADA.jpg"
+import IMG38 from "../assets/images/products/Group-three/กระจกโค้งจราจรแบบมีหมวก ขนาด 24 YAMADA.jpg"
+import IMG39 from "../assets/images/products/Group-three/กระจกโค้งจราจรแบบมีหมวก ขนาด 32 YAMADA.jpg"
+import IMG40 from "../assets/images/products/Group-three/กระจกโค้งจราจรแบบมีหมวก ขนาด 40 YAMADA.jpg"
+
+export const products = [
+    {
+        id: 1,
+        name: "สเปรย์ฉีด",
+        category: "ถังดับเพลิง",
+        // size: '2.2l',
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG1,
+        imageAlt: "spray",
+    },
+    {
+        id: 2,
+        name: "ถังดับเพลิง 2.2 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "2.2P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG2,
+        imageAlt: "fireman 2.2P",
+    },
+    {
+        id: 3,
+        name: "ถังดับเพลิง 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG3,
+        imageAlt: "fireman 5P",
+    },
+    {
+        id: 4,
+        name: "ถังดับเพลิง 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG4,
+        imageAlt: "fireman10ปอนด์",
+    },
+    {
+        id: 5,
+        name: "ถังดับเพลง 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG5,
+        imageAlt: "fireman15ปอนด์",
+    },
+    {
+        id: 6,
+        name: "ถังดับเพลิง ชนิดน้ำสะสมแรงดัน 6 ลิตร",
+        category: "ถังดับเพลิง",
+        size: "6l",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG6,
+        imageAlt: "fireman (AFFF Pre-Mix Foam) 6 ลิตร",
+    },
+    {
+        id: 7,
+        name: "ถังดับเพลิง ชนิดน้ำสะสมแรงดัน 9 ลิตร",
+        category: "ถังดับเพลิง",
+        size: "9l",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG7,
+        imageAlt: "fireman (AFFF Pre-Mix Foam) 9 ลิตร",
+    },
+    {
+        id: 8,
+        name: "ถังดับเพลิง ชนิดสารระเหย 2.2 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "2.2P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG8,
+        imageAlt: "fireman BF2000 2.2 ปอนด์",
+    },
+    {
+        id: 9,
+        name: "ถังดับเพลิง ชนิดสารระเหย 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG9,
+        imageAlt: "fireman BF2000 5 ปอนด์",
+    },
+    {
+        id: 10,
+        name: "ถังดับเพลิง ชนิดสารระเหย 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG10,
+        imageAlt: "fireman BF2000 10 ปอนด์",
+    },
+    {
+        id: 11,
+        name: "ถังดับเพลิง ชนิดสารระเหย 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG11,
+        imageAlt: "fireman BF2000 15 ปอนด์",
+    },
+    {
+        id: 12,
+        name: "ถังดับเพลิง ชนิด Co2 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG12,
+        imageAlt: "fireman Co2 5 ปอนด์.jpg",
+    },
+    {
+        id: 13,
+        name: "ถังดับเพลิง ชนิด Co2 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG13,
+        imageAlt: "fireman Co2 10 ปอนด์.jpg",
+    },
+    {
+        id: 14,
+        name: "ถังดับเพลิง ชนิด Co2 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG14,
+        imageAlt: "fireman Co2 15 ปอนด์",
+    },
+    {
+        id: 15,
+        name: "ถังดับเพลิง ชนิด Water Gas 6 ลิตร",
+        category: "ถังดับเพลิง",
+        size: "6l",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG15,
+        imageAlt: "fireman (Water Gas) ขนาด 6 ลิตร",
+    },
+    {
+        id: 16,
+        name: "ถังดับเพลิง ชนิด Water Gas 9 ลิตร",
+        category: "ถังดับเพลิง",
+        size: "9l",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG16,
+        imageAlt: "fireman (Water Gas) ขนาด 9 ลิตร",
+    },
+    {
+        id: 17,
+        name: "ถังดับเพลิง Firemax 2.2 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "2.2P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG17,
+        imageAlt: "firemax2.2 ปอนด์",
+    },
+    {
+        id: 18,
+        name: "ถังดับเพลิง Firemax 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG18,
+        imageAlt: "firemax5 ปอนด์",
+    },
+    {
+        id: 19,
+        name: "ถังดับเพลิง Firemax 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG19,
+        imageAlt: "firemax 10 ปอนด์",
+    },
+    {
+        id: 20,
+        name: "ถังดับเพลิง Firemax 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG20,
+        imageAlt: "firemax5 ปอนด์",
+    },
+    {
+        id: 21,
+        name: "ถังดับเพลิง Firemax 20 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "20P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG21,
+        imageAlt: "firemax20 ปอนด์",
+    },
+    {
+        id: 22,
+        name: "ถังดับเพลิง Firemax BF2000 2.2 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "2.2P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG22,
+        imageAlt: "firemax BF2000 2.2 ปอนด์",
+    },
+    {
+        id: 23,
+        name: "ถังดับเพลิง Firemax BF2000 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG23,
+        imageAlt: "firemax BF2000 5 ปอนด์",
+    },
+    {
+        id: 24,
+        name: "ถังดับเพลิง Firemax BF2000 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG24,
+        imageAlt: "firemax BF2000 10 ปอนด์",
+    },
+    {
+        id: 25,
+        name: "ถังดับเพลิง Firemax BF2000 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG25,
+        imageAlt: "firemax BF2000 15 ปอนด์",
+    },
+    {
+        id: 26,
+        name: "ถังดับเพลิง Co2 5 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "5P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG26,
+        imageAlt: "firemax Co2 5 ปอนด์",
+    },
+    {
+        id: 27,
+        name: "ถังดับเพลิง Co2 10 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "10P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG27,
+        imageAlt: "firemax Co2 10 ปอนด์",
+    },
+    {
+        id: 28,
+        name: "ถังดับเพลิง Co2 15 ปอนด์",
+        category: "ถังดับเพลิง",
+        size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG28,
+        imageAlt: "firemax Co2 15 ปอนด์",
+    },
+    {
+        id: 29,
+        name: "ไฟฉุกเฉิน CU209CD4",
+        category: "ไฟฉุกเฉิน",
+        // size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG29,
+        imageAlt: "sunny CU209CD4",
+    },
+    {
+        id: 30,
+        name: "ไฟฉุกเฉิน SG209CD2",
+        category: "ไฟฉุกเฉิน",
+        // size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG30,
+        imageAlt: "sunny SG209CD2",
+    },
+    {
+        id: 31,
+        name: "ไฟฉุกเฉิน SG209CD4",
+        category: "ไฟฉุกเฉิน",
+        // size: "15P",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG31,
+        imageAlt: "sunny SG209CD4",
+    },
+    {
+        id: 32,
+        name: "กรวยจราจร 30 เซนติเมตร",
+        category: "อุปกรณ์จราจร",
+        size: "30cm",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG32,
+        imageAlt: "กรวยจราจร 30cm.jpg",
+    },
+    {
+        id: 33,
+        name: "กรวยจราจร 50 เซนติเมตร",
+        category: "อุปกรณ์จราจร",
+        size: "50cm",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG33,
+        imageAlt: "กรวยจราจร 50cm.jpg",
+    },
+    {
+        id: 34,
+        name: "กรวยจราจร 70 เซนติเมตร",
+        category: "อุปกรณ์จราจร",
+        size: "70cm",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG34,
+        imageAlt: "กรวยจราจร 70cm.jpg",
+    },
+    {
+        id: 35,
+        name: "กรวยจราจร 80 เซนติเมตร",
+        category: "อุปกรณ์จราจร",
+        size: "80cm",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG35,
+        imageAlt: "กรวยจราจร 80cm.jpg",
+    },
+    {
+        id: 36,
+        name: "กระจกโค้งจราจรแบบมีหมวก YAMADA ขนาด 12 นิ้ว",
+        category: "อุปกรณ์จราจร",
+        size: "12Inc",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG36,
+        imageAlt: "กระจกโค้งจราจรแบบมีหมวก ขนาด 12 YAMADA",
+    },
+    {
+        id: 37,
+        name: "กระจกโค้งจราจรแบบมีหมวก YAMADA ขนาด 18 นิ้ว",
+        category: "อุปกรณ์จราจร",
+        size: "18Inc",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG37,
+        imageAlt: "กระจกโค้งจราจรแบบมีหมวก ขนาด 18 YAMADA",
+    },
+    {
+        id: 38,
+        name: "กระจกโค้งจราจรแบบมีหมวก YAMADA ขนาด 24 นิ้ว",
+        category: "อุปกรณ์จราจร",
+        size: "24Inc",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG38,
+        imageAlt: "กระจกโค้งจราจรแบบมีหมวก ขนาด 24 YAMADA",
+    },
+    {
+        id: 39,
+        name: "กระจกโค้งจราจรแบบมีหมวก YAMADA ขนาด 32 นิ้ว",
+        category: "อุปกรณ์จราจร",
+        size: "32Inc",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG39,
+        imageAlt: "กระจกโค้งจราจรแบบมีหมวก ขนาด 32 YAMADA",
+    },
+    {
+        id: 40,
+        name: "กระจกโค้งจราจรแบบมีหมวก YAMADA ขนาด 40 นิ้ว",
+        category: "อุปกรณ์จราจร",
+        size: "40Inc",
+        // to: "/products",
+        // price: '$48',
+        imageSrc: IMG40,
+        imageAlt: "กระจกโค้งจราจรแบบมีหมวก ขนาด 40 YAMADA",
+    },
+];
+
+export const subCategories = [
+    { name: "ถังดับเพลิง", href: "#" },
+    { name: "ไฟฉุกเฉิน", href: "#" },
+    // { name: "ป้ายไฟฉุกเฉิน", href: "#" },
+    { name: "อุปกรณ์จราจร", href: "#" },
+    // { name: "accessory", href: "#" },
+];
+
+export const filters = [
+    {
+        id: "size",
+        name: "Size",
+        options: [
+            { value: "30cm", label: "30cm", checked: false},
+            { value: "50cm", label: "50cm", checked: false},
+            { value: "70cm", label: "70cm", checked: false},
+            { value: "80cm", label: "80cm", checked: false},
+            { value: "12Inc", label: "12Inc", checked: false},
+            { value: "18Inc", label: "18Inc", checked: false},
+            { value: "24Inc", label: "24Inc", checked: false},
+            { value: "32Inc", label: "32Inc", checked: false},
+            { value: "40Inc", label: "40Inc", checked: false},
+            { value: "6l", label: "6l", checked: false },
+            { value: "9l", label: "9l", checked: false },
+            { value: "2.2P", label: "2.2P", checked: false },
+            { value: "5P", label: "5P", checked: false },
+            { value: "10P", label: "10P", checked: false },
+            { value: "15P", label: "15P", checked: false },
+            { value: "20P", label: "20P", checked: false },
+        ],
+    },
+];
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -6,77 +6,9 @@ import { Dialog, DialogBackdrop, DialogPanel, Disclosure, DisclosureButton, Disc
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { FunnelIcon, MinusIcon, PlusIcon } from '@heroicons/react/20/solid'
 
-// images
-import IMG1 from '../assets/images/home/A0149467.jpg'
-import IMG2 from '../assets/images/home/A0148264.jpg'
-import IMG3 from '../assets/images/home/A0149476.jpg'
-import IMG4 from '../assets/images/home/A0158296.jpg'
 
-const subCategories = [
-    { name: 'ถังดับเพลิง', href: '#' },
-    { name: 'ไฟฉุกเฉิน', href: '#' },
-    { name: 'ป้ายไฟฉุกเฉิน', href: '#' },
-    { name: 'อุปกรณ์จราจร', href: '#' },
-    { name: 'accessory', href: '#' },
-]
-
-const filters = [
-    {
-        id: 'size',
-        name: 'Size',
-        options: [
-            { value: '2l', label: '2L', checked: false },
-            { value: '6l', label: '6L', checked: false },
-            { value: '12l', label: '12L', checked: false },
-            { value: '18l', label: '18L', checked: false },
-            { value: '20l', label: '20L', checked: false },
-            { value: '40l', label: '40L', checked: true },
-        ],
-    },
-]
-
-const products = [
-    {
-        id: 1,
-        name: 'ไฟฉุกเฉิน LED 12V 9W',
-        category: 'ไฟฉุกเฉิน',
-        size: '12l',
-        to: "/products",
-        price: '$48',
-        imageSrc: IMG1,
-        imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-    },
-    {
-        id: 2,
-        name: 'สเปร์ยดับเพลิง 500 มล.',
-        category: 'accessory',
-        size: '6l',
-        to: "/products",
-        price: '$35',
-        imageSrc: IMG3,
-        imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
-    },
-    {
-        id: 3,
-        name: 'ถังดับเพลิงแบบเคมีแห้งชนิดเติมได้ 10 ปอนด์',
-        category: 'ถังดับเพลิง',
-        size: '20l',
-        to: "/products",
-        price: '$89',
-        imageSrc: IMG2,
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
-    },
-    {
-        id: 4,
-        name: 'เครื่องดับเพลิง ชนิดเคมีแห้ง Rating 4A5B ขนาด 10 ปอนด์',
-        category: 'ถังดับเพลิง',
-        size: '40l',
-        to: "/products",
-        price: '$35',
-        imageSrc: IMG4,
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-]
+// dbproductgroup
+import { products, subCategories, filters } from '../data/ProductPage'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -248,7 +180,7 @@ const ProductsPage = () => {
                                         <Link to={product.to} className="group" key={product.id}>
                                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                                 <img src={product.imageSrc} alt={product.imageAlt} className="h-full w-full object-cover object-center group-hover:opacity-75" />
-                                                <button className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity">ดูรายละเอียดเพิ่มเติม</button>
+                                                {/* <button className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity">ดูรายละเอียดเพิ่มเติม</button> */}
                                             </div>
                                             <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                                             <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
